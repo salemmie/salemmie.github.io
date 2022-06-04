a4ee08fc57ef6b7b20b4dd5cb0f4b16817cfa324
madi0cre <100317615+madi0cre@users.noreply.github.com>
2022-06-04 15:47:11 -0400

    new site

---
+++ projects/Whos That Pokemon?/index.js
@@ -0,0 +1,9 @@
+let gameData;
+const main = document.querySelector('main');
+const playBtn = document.querySelector('#play');
+
+playBtn.addEventListener('click', fetchData);
+
+async function fetchData() {
+    gameData = await window.getPokeData();
+}
